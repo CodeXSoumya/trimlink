b15afae0eb3f623bd3997c0712bd08580846c1b5
soumya1436 <soumya.a.mitra@capgemini.com>
2026-05-28 11:34:08 +0530

    Backend build fix

--- src/test/java/com/codex/trimlink/TrimlinkApplicationTests.java
+++ src/test/java/com/codex/trimlink/TrimlinkApplicationTests.java
@@ -3,7 +3,11 @@ package com.codex.trimlink;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "zookeeper.connectionString=localhost:2181",
+    "spring.data.redis.host=localhost"
+})
 class TrimlinkApplicationTests {
 
 	@Test
