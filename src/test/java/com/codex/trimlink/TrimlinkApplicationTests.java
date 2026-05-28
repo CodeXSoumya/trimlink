177026a7a7b5a7d5feae7baaf6cc20483033aa47
soumya1436 <soumya.a.mitra@capgemini.com>
2026-05-28 12:06:44 +0530

    Test fix attempt 3

--- src/test/java/com/codex/trimlink/TrimlinkApplicationTests.java
+++ src/test/java/com/codex/trimlink/TrimlinkApplicationTests.java
@@ -1,6 +1,8 @@
 package com.codex.trimlink;
 
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 @ActiveProfiles("test")
